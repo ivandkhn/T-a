f353dc955e37f95b31c6bbab05d3cc9f11570e5d
johndahrah <dahrah@yandex.ru>
2019-05-19 22:12:32 +0300

    Initial Commit

---
+++ T-a/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  T-a
+//
+//  Created by Иван Дахненко on 19/05/2019.
+//  Copyright © 2019 Ivan Dakhnenko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
